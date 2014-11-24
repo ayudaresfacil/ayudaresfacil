dd0c6dad03d02f6234b5f119f988fa229280edbb
Sebastin M. Perez <perezsebastianm@gmail.com>
2014-11-24 16:32:31 -0300

    Cambio tabulacion a 4 espacios

--- client/src/app/offer/offer.js
+++ client/src/app/offer/offer.js
@@ -1,129 +1,150 @@
-angular.module( 'AyudarEsFacilApp.offer', [
-  'ui.router','ui.bootstrap'
-  ])
+angular.module('AyudarEsFacilApp.offer', [
+    'ui.router', 'ui.bootstrap'
+])
 
-.config(function config( $stateProvider ) {
-  $stateProvider.state( 'web.offerList', {        
-    url: '/ofrecimientos',
-    controller: 'OfferCtrl',
-    templateUrl: 'offer/offer-list.tpl.html',
-    data:{ pageTitle: 'Ofrecimientos' }
-  });
-  $stateProvider.state( 'panel.offerCreate', {        
-    url: '/ofrecer-ayuda',
-    controller: 'OfferCreateCtrl',
-    templateUrl: 'offer/offer-create.tpl.html',
-    data:{ pageTitle: 'Crear Ofrecimiento' }
-  });
-  $stateProvider.state( 'web.offerDetail', {     
-    url: '/ofrecimiento-detalle/:id',
-    controller: 'OfferCtrl',
-    templateUrl: 'offer/offer-detail.tpl.html',
-    data:{ pageTitle: 'Detalle del Ofrecimiento' }
-  });
+.config(function config($stateProvider) {
+    $stateProvider.state('web.offerList', {
+        url: '/ofrecimientos',
+        controller: 'OfferCtrl',
+        templateUrl: 'offer/offer-list.tpl.html',
+        data: {
+            pageTitle: 'Ofrecimientos'
+        }
+    });
+    $stateProvider.state('panel.offerCreate', {
+        url: '/ofrecer-ayuda',
+        controller: 'OfferCreateCtrl',
+        templateUrl: 'offer/offer-create.tpl.html',
+        data: {
+            pageTitle: 'Crear Ofrecimiento'
+        }
+    });
+    $stateProvider.state('web.offerDetail', {
+        url: '/ofrecimiento-detalle/:id',
+        controller: 'OfferCtrl',
+        templateUrl: 'offer/offer-detail.tpl.html',
+        data: {
+            pageTitle: 'Detalle del Ofrecimiento'
+        }
+    });
 })
 
 // Authentication service for user variables
 .factory('Authentication', [
 
-  function() {
-    var _this = this;
+    function() {
+        var _this = this;
 
-    _this._data = {
-      user: JSON.parse(localStorage.getItem("user"))
-    };
+        _this._data = {
+            user: JSON.parse(localStorage.getItem("user"))
+        };
 
-    return _this._data;
-  }
-  ])
+        return _this._data;
+    }
+])
 
 // Users service used for communicating with the users REST endpoint
 .factory('Offers', ['$resource',
-  function($resource) {
-    return $resource('http://localhost/ayudaresfacil/api/offer', {publicationId:'@id'}, {}, {
-      update: {
-        method: 'PUT'
-      }
-    });
-  }
-  ])
+    function($resource) {
+        return $resource('http://localhost/ayudaresfacil/api/offer', {
+            publicationId: '@id'
+        }, {}, {
+            update: {
+                method: 'PUT'
+            }
+        });
+    }
+])
 
 .factory('Favorites', ['$resource',
-  function($resource) {
-    return $resource('http://localhost/ayudaresfacil/api/offer/favorite', {}, {
-      update: {
-        method: 'PUT'
-      }
-    });
-  }
-  ])
-
-.controller( 'OfferCtrl', function OfferCtrl( $scope, $http, Offers, $stateParams, Favorites, Authentication){
-  $scope.myInterval = 5000;
-  $scope.user = Authentication.user;
-
-  var offers = new Offers();
-  var favorites = new Favorites();
-  
-  if ($stateParams.id === undefined){
-    if (Authentication.user === null){
-      offers.$get(function(response){
-        $scope.offers = offers.data;
-      });
-    }else{
-      offers.$get({userLog:Authentication.user.id}, function(response){
-        $scope.offers = offers.data;
-      });
+    function($resource) {
+        return $resource('http://localhost/ayudaresfacil/api/offer/favorite', {}, {
+            update: {
+                method: 'PUT'
+            }
+        });
     }
-  }else{
-    if (Authentication.user === null){
-      offers.$get({publicationId:$stateParams.id},function(response){
-        $scope.offers = offers.data;
-      });
-    }else{
-      offers.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-        $scope.offers = offers.data;
-      });
-    }
-  }
+])
 
-  $scope.setFavorite = function(id) {
-    var data = {
-      publicationId: id, 
-      userId: $scope.user.id
+.controller('OfferCtrl', function OfferCtrl($scope, $http, Offers, $stateParams, Favorites, Authentication) {
+    $scope.myInterval = 5000;
+    $scope.user = Authentication.user;
+
+    var offers = new Offers();
+    var favorites = new Favorites();
+
+    if ($stateParams.id === undefined) {
+        if (Authentication.user === null) {
+            offers.$get(function(response) {
+                $scope.offers = offers.data;
+            });
+        } else {
+            offers.$get({
+                userLog: Authentication.user.id
+            }, function(response) {
+                $scope.offers = offers.data;
+            });
+        }
+    } else {
+        if (Authentication.user === null) {
+            offers.$get({
+                publicationId: $stateParams.id
+            }, function(response) {
+                $scope.offers = offers.data;
+            });
+        } else {
+            offers.$get({
+                userLog: Authentication.user.id,
+                publicationId: $stateParams.id
+            }, function(response) {
+                $scope.offers = offers.data;
+            });
+        }
+    }
+
+    $scope.setFavorite = function(id) {
+        var data = {
+            publicationId: id,
+            userId: $scope.user.id
+        };
+
+        $http.post('/ayudaresfacil/api/offer/favorite', data)
+            .success(function(response) {
+                $scope.error = false;
+                offers.$get({
+                    userLog: Authentication.user.id,
+                    publicationId: $stateParams.id
+                }, function(response) {
+                    $scope.offers = offers.data;
+                });
+            })
+            .error(function(response) {
+                $scope.error = true;
+                $scope.credentials = {};
+            });
     };
 
-    $http.post('/ayudaresfacil/api/offer/favorite', data)
-    .success(function(response) {
-      $scope.error = false;
-      offers.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-        $scope.offers = offers.data;
-      });
-    })
-    .error(function(response) { 
-      $scope.error = true;
-      $scope.credentials = {};
-    });
-  };
+    $scope.unsetFavorite = function(id) {
+        var data = {
+            publicationId: id,
+            userId: $scope.user.id,
+            del: 'true'
+        };
 
-  $scope.unsetFavorite = function(id) {
-    var data = {
-      publicationId: id, 
-      userId: $scope.user.id,
-      del:'true'
+        $http.post('/ayudaresfacil/api/offer/favorite', data)
+            .success(function(response) {
+                $scope.error = false;
+                offers.$get({
+                    userLog: Authentication.user.id,
+                    publicationId: $stateParams.id
+                }, function(response) {
+                    $scope.offers = offers.data;
+                });
+            })
+            .error(function(response) {
+                $scope.error = true;
+                $scope.credentials = {};
+            });
     };
-
-    $http.post('/ayudaresfacil/api/offer/favorite', data)
-    .success(function(response) {
-      $scope.error = false;
-      offers.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-        $scope.offers = offers.data;
-      });
-    })
-    .error(function(response) { 
-      $scope.error = true;
-      $scope.credentials = {};
-    });
-  };
 
 });
--- client/src/app/request/request.js
+++ client/src/app/request/request.js
@@ -1,156 +1,183 @@
-angular.module( 'AyudarEsFacilApp.request', [
-  'ui.router'
-  ])
+angular.module('AyudarEsFacilApp.request', [
+    'ui.router'
+])
 
-.config(function config( $stateProvider ) {
-  $stateProvider.state( 'web.requestList', {        
-    url: '/pedidos',
-    controller: 'RequestCtrl',
-    templateUrl: 'request/request-list.tpl.html',
-    data:{ pageTitle: 'Pedidos' }
-  });
-  $stateProvider.state( 'panel.requestCreate', {        
-    url: '/pedir-ayuda',
-    controller: 'RequestCreateCtrl',
-    templateUrl: 'request/request-create.tpl.html',
-    data:{ pageTitle: 'Crear Pedido' }
-  });
-  $stateProvider.state( 'web.requestDetail', {     
-    url: '/pedido-detalle/:id',
-    controller: 'RequestCtrl',
-    templateUrl: 'request/request-detail.tpl.html',
-    data:{ pageTitle: 'Detalle del Pedido' }
-  });
+.config(function config($stateProvider) {
+    $stateProvider.state('web.requestList', {
+        url: '/pedidos',
+        controller: 'RequestCtrl',
+        templateUrl: 'request/request-list.tpl.html',
+        data: {
+            pageTitle: 'Pedidos'
+        }
+    });
+    $stateProvider.state('panel.requestCreate', {
+        url: '/pedir-ayuda',
+        controller: 'RequestCreateCtrl',
+        templateUrl: 'request/request-create.tpl.html',
+        data: {
+            pageTitle: 'Crear Pedido'
+        }
+    });
+    $stateProvider.state('web.requestDetail', {
+        url: '/pedido-detalle/:id',
+        controller: 'RequestCtrl',
+        templateUrl: 'request/request-detail.tpl.html',
+        data: {
+            pageTitle: 'Detalle del Pedido'
+        }
+    });
 })
 
 // Authentication service for user variables
 .factory('Authentication', [
 
-  function() {
-    var _this = this;
+    function() {
+        var _this = this;
 
-    _this._data = {
-      user: JSON.parse(localStorage.getItem("user"))
-    };
+        _this._data = {
+            user: JSON.parse(localStorage.getItem("user"))
+        };
 
-    return _this._data;
-  }
-  ])
+        return _this._data;
+    }
+])
 
 // Users service used for communicating with the users REST endpoint
 .factory('Requests', ['$resource',
-  function($resource) {
-    return $resource('http://localhost/ayudaresfacil/api/request', {publicationId:'@id'}, {}, {
-      update: {
-        method: 'PUT'
-      }
-    });
-  }
-  ])
-
-.controller( 'RequestCtrl', function RequestCtrl( $scope, $http, Requests, $stateParams, Authentication) {
-  $scope.myInterval = 5000;
-  $scope.user = Authentication.user;
-
-  var requests = new Requests();
-  
-  if ($stateParams.id === undefined){
-    if (Authentication.user === null){
-      requests.$get(function(response){
-        $scope.requests = requests.data;
-      });
-    }else{
-      requests.$get({userLog:Authentication.user.id}, function(response){
-        $scope.requests = requests.data;
-      });
+    function($resource) {
+        return $resource('http://localhost/ayudaresfacil/api/request', {
+            publicationId: '@id'
+        }, {}, {
+            update: {
+                method: 'PUT'
+            }
+        });
     }
-  }else{
-    if (Authentication.user === null){
-      requests.$get({publicationId:$stateParams.id},function(response){
-        $scope.requests = requests.data;
-      });
-    }else{
-      requests.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-        $scope.requests = requests.data;
-      });
-    }    
-  }
+])
 
-$scope.setFavorite = function(id) {
-  var data = {
-    publicationId: id, 
-    userId: $scope.user.id
-  };
+.controller('RequestCtrl', function RequestCtrl($scope, $http, Requests, $stateParams, Authentication) {
+    $scope.myInterval = 5000;
+    $scope.user = Authentication.user;
 
-  $http.post('/ayudaresfacil/api/request/favorite', data)
-  .success(function(response) {
-    $scope.error = false;
-    requests.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-      $scope.requests = requests.data;
-    });
-  })
-  .error(function(response) { 
-    $scope.error = true;
-    $scope.credentials = {};
-  });
-};
+    var requests = new Requests();
 
-$scope.unsetFavorite = function(id) {
-  var data = {
-    publicationId: id, 
-    userId: $scope.user.id,
-    del:'true'
-  };
+    if ($stateParams.id === undefined) {
+        if (Authentication.user === null) {
+            requests.$get(function(response) {
+                $scope.requests = requests.data;
+            });
+        } else {
+            requests.$get({
+                userLog: Authentication.user.id
+            }, function(response) {
+                $scope.requests = requests.data;
+            });
+        }
+    } else {
+        if (Authentication.user === null) {
+            requests.$get({
+                publicationId: $stateParams.id
+            }, function(response) {
+                $scope.requests = requests.data;
+            });
+        } else {
+            requests.$get({
+                userLog: Authentication.user.id,
+                publicationId: $stateParams.id
+            }, function(response) {
+                $scope.requests = requests.data;
+            });
+        }
+    }
 
-  $http.post('/ayudaresfacil/api/request/favorite', data)
-  .success(function(response) {
-    $scope.error = false;
-    requests.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-      $scope.requests = requests.data;
-    });
-  })
-  .error(function(response) { 
-    $scope.error = true;
-    $scope.credentials = {};
-  });
-};
+    $scope.setFavorite = function(id) {
+        var data = {
+            publicationId: id,
+            userId: $scope.user.id
+        };
 
-$scope.setVote = function(id) {
-  var data = {
-    publicationId: id, 
-    userId: $scope.user.id
-  };
+        $http.post('/ayudaresfacil/api/request/favorite', data)
+            .success(function(response) {
+                $scope.error = false;
+                requests.$get({
+                    userLog: Authentication.user.id,
+                    publicationId: $stateParams.id
+                }, function(response) {
+                    $scope.requests = requests.data;
+                });
+            })
+            .error(function(response) {
+                $scope.error = true;
+                $scope.credentials = {};
+            });
+    };
 
-  $http.post('/ayudaresfacil/api/request/vote', data)
-  .success(function(response) {
-    $scope.error = false;
-    requests.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-      $scope.requests = requests.data;
-    });
-  })
-  .error(function(response) { 
-    $scope.error = true;
-    $scope.credentials = {};
-  });
-};
+    $scope.unsetFavorite = function(id) {
+        var data = {
+            publicationId: id,
+            userId: $scope.user.id,
+            del: 'true'
+        };
 
-$scope.unsetVote = function(id) {
-  var data = {
-    publicationId: id, 
-    userId: $scope.user.id,
-    del:'true'
-  };
+        $http.post('/ayudaresfacil/api/request/favorite', data)
+            .success(function(response) {
+                $scope.error = false;
+                requests.$get({
+                    userLog: Authentication.user.id,
+                    publicationId: $stateParams.id
+                }, function(response) {
+                    $scope.requests = requests.data;
+                });
+            })
+            .error(function(response) {
+                $scope.error = true;
+                $scope.credentials = {};
+            });
+    };
 
-  $http.post('/ayudaresfacil/api/request/vote', data)
-  .success(function(response) {
-    $scope.error = false;
-    requests.$get({userLog:Authentication.user.id, publicationId:$stateParams.id},function(response){
-      $scope.requests = requests.data;
-    });
-  })
-  .error(function(response) { 
-    $scope.error = true;
-    $scope.credentials = {};
-  });
-};
+    $scope.setVote = function(id) {
+        var data = {
+            publicationId: id,
+            userId: $scope.user.id
+        };
+
+        $http.post('/ayudaresfacil/api/request/vote', data)
+            .success(function(response) {
+                $scope.error = false;
+                requests.$get({
+                    userLog: Authentication.user.id,
+                    publicationId: $stateParams.id
+                }, function(response) {
+                    $scope.requests = requests.data;
+                });
+            })
+            .error(function(response) {
+                $scope.error = true;
+                $scope.credentials = {};
+            });
+    };
+
+    $scope.unsetVote = function(id) {
+        var data = {
+            publicationId: id,
+            userId: $scope.user.id,
+            del: 'true'
+        };
+
+        $http.post('/ayudaresfacil/api/request/vote', data)
+            .success(function(response) {
+                $scope.error = false;
+                requests.$get({
+                    userLog: Authentication.user.id,
+                    publicationId: $stateParams.id
+                }, function(response) {
+                    $scope.requests = requests.data;
+                });
+            })
+            .error(function(response) {
+                $scope.error = true;
+                $scope.credentials = {};
+            });
+    };
 });
