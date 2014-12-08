9d8a9d1366676d4a9181ef66657bb538974885bc
Sebastin M. Perez <perezsebastianm@gmail.com>
2014-12-08 18:03:23 -0300

    Agrego fin del proceso de checkout para las publicaciones de ofertas

--- api/application/helpers/authentication_helper.php
+++ api/application/helpers/authentication_helper.php
@@ -1,6 +1,18 @@
 <?php
 	function checkIsLoggedIn($self){
-		if(!CI_Auth::isLoggedIn($self->get('token')))
+        $token = "";
+
+        if(!empty($self->get('token'))){
+            $token = $self->get('token');
+        }elseif(!empty($self->post('token'))){
+            $token = $self->post('token');
+        }elseif(!empty($self->put('token'))){
+            $token = $self->put('token');
+        }elseif(!empty($self->delete('token'))){
+            $token = $self->delete('token');
+        }
+
+		if(!CI_Authentication::isLoggedIn($token))
 			$self->response(array('status' => 'UNAUTHORIZED'), 401);		
 	}
 ?>
